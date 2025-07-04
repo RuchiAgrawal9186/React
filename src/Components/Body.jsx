39fa1e4d85bb94d269512f93a30a1c73d6e3ad43
RuchiAgrawal01 <ruchi.agrawal@portqii.com>
2025-07-04 16:50:31 +0530

    adding fetch api and show cards

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestorantCard from "./RestorantCard";
 import { cards } from "../Utils/mockData";
 
 const Body = () => {
-  const [topRatingCard,setTopRatingCard]=useState([...cards])
+  const [topRatingCard, setTopRatingCard] = useState([...cards]);
+
+  // after render component we want to again render we can use useEffect
+
+  const fetchData = async () => {
+    let res = await fetch(
+      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+    );
+    let data = await res.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
   return (
     <div className="body">
       <div className="search">search</div>
-      <button onClick={()=> {
-        const filterdCard = topRatingCard?.filter((el)=> el?.card?.card?.info?.avgRating > 4.5)
-        setTopRatingCard(filterdCard)
-          
-      }}>Top rating</button>
+      <button
+        onClick={() => {
+          const filterdCard = topRatingCard?.filter(
+            (el) => el?.card?.card?.info?.avgRating > 4.5
+          );
+          setTopRatingCard(filterdCard);
+        }}
+      >
+        Top rating
+      </button>
       <div className="res-container">
         {topRatingCard?.map((el, index) => {
           return (
